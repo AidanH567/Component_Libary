24925a30cd17a12af587aaad0c3922962dc2a58f
AidanH567 <herstikaidan@gmail.com>
2025-12-09 19:29:07 +0100

    setting up project

--- index.jsx
+++ index.jsx
@@ -1,7 +1,5 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Menu from "./components/Menu/index"
-import Star from "./components/Star"
+
 
 function App() {
   return (
